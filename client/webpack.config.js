640e0d718fbe2b641a3333eaf4068744fdab9f8b
stephen-castillo <122939431+stephen-castillo@users.noreply.github.com>
2023-06-05 23:32:56 -0500

    Udpated webpack config and ran npm run build

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,11 +4,14 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 // TODO: Add CSS loaders and babel to webpack.
+const BabelLoader = require('babel-loader');
+
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: 'production',
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js'
@@ -18,12 +21,44 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+        new WebpackPwaManifest({
+            name: 'Your App Name',
+            short_name: 'App',
+            description: 'Your App Description',
+            background_color: '#ffffff',
+            crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+            icons: [
+                {
+                    src: path.resolve('src/images/logo.png'),
+                    sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+                },
+            ],
+        }),
+        new InjectManifest({
+            swSrc: './src-sw.js',
+        }),
+        new MiniCssExtractPlugin(),
     ],
 
     module: {
       rules: [
-        
+        {
+            test: /\.css$/,
+            use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader",
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            },
+        },
       ],
     },
   };
