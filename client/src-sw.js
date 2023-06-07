665927092831c984bd1b4dec36e4328146d9ae14
stephen-castillo <122939431+stephen-castillo@users.noreply.github.com>
2023-06-07 01:44:22 -0500

    Re-did the registerRoute function because it was not working

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,28 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+const assetCache = new CacheFirst({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 50, // Maximum number of entries this cache can hold.
+        maxAgeSeconds: 30 * 24 * 60 * 60, // Cache for a maximum of 30 days.
+      }),
+    ],
+  });
+  
+  // Cache .js, .css, and image files.
+registerRoute(
+    ({ request }) => ["style", "script", "worker"].includes(request.destination),
+    new StaleWhileRevalidate({
+        cacheName: "asset-cache",
+        plugins: [
+            new CacheableResponsePlugin({
+                statuses: [0, 200],
+            }),
+        ],
+    })
+);
