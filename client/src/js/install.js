8af8a6ce8cf211b83ad8cbdf494c91468344d9c1
stephen-castillo <122939431+stephen-castillo@users.noreply.github.com>
2023-06-07 01:31:17 -0500

    Re-hidding the install button after installing pwa

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,39 @@
 const butInstall = document.getElementById('buttonInstall');
 
+let deferredPrompt;
+
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    // Prevent Chrome from showing the default prompt.
+    //event.preventDefault();
+
+    // Stash the event so it can be triggered later.
+    window.deferredPrompt = event;
+
+    // Update UI notify the user they can install the PWA
+    butInstall.classList.toggle('hidden', false);
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    // Show the prompt
+    const promptEvent = window.deferredPrompt;
+
+    if (!promptEvent) {
+        // The deferred prompt isn't available.
+        return;
+    }
+    promptEvent.prompt();
+
+    // We no longer need the deferredPrompt. Clear it up.
+    window.deferredPrompt = null;
+
+    butInstall.classList.toggle('hidden', true);
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    console.log('PWA was installed');
+    window.deferredPrompt = null;
+});
